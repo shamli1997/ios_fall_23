76e023cf8569182ac6c1194b8c0309368ea5f9da
Shamli Ingole <54771718+shamli1997@users.noreply.github.com>
2023-07-13 15:15:21 -0700

    Added pre-work for IOS

--- codepath_ios/ViewController.swift
+++ codepath_ios/ViewController.swift
@@ -13,7 +13,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    func changeColor() -> UIColor{
 
+           let red = CGFloat.random(in: 0...1)
+           let green = CGFloat.random(in: 0...1)
+           let blue = CGFloat.random(in: 0...1)
 
+           return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+       }
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+        let labelColor = changeColor()
+        label1.textColor = labelColor
+        label2.textColor = labelColor
+        label3.textColor = labelColor
+    }
 }
 
